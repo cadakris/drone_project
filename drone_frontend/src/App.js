6da1fe1944795f346b5a26f8d258b0b1a8d338ee
Kristen <kriscadacio@gmail.com>
2024-11-24 21:34:55 -0500

    playing around with Tailwindcss code

--- drone_frontend/src/App.js
+++ drone_frontend/src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import ProjectPosts from './components/ProjectPost';
 
+
 function App() {
   return (
     <div className="App">
-    <h1>HELLOOO</h1>
+    <h1  class="text-amber-800">HELLOOO</h1>
     <ProjectPosts /> {/* Add ProjectPosts component here */}
   </div>
   );

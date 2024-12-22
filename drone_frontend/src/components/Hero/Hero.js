d8ce837b453b4fe3d1721b9feedc58096985c2b8
Kristen <kriscadacio@gmail.com>
2024-12-21 23:48:29 -0500

    Added Hero component holding left and right containers

--- drone_frontend/src/components/Hero/Hero.js
+++ drone_frontend/src/components/Hero/Hero.js
@@ -21,7 +21,33 @@ function Hero() {
     };
 
   return (
-    <div>Hero</div>
+    <Fade top>
+        <div
+            className="main-hero-container"
+            id="main-page"
+            onMouseLeave={handleMouseLeave}
+        >
+            <div
+                className={`hover-left-2 ${hoveredSide == 'left' ? 'expanded' : ''}`}
+                onMouseEnter={handleMouseEnterLeft}
+            >
+                <div className="parallax">
+                    <LeftContainer />
+                </div>
+            </div>
+    
+            <div
+                className={`hover-left-2 ${hoveredSide == 'right' ? 'expanded' : ''}`}
+                onMouseEnter={handleMouseEnterRight}
+            >
+                <div className='parallax'>
+                    <RightContainer />
+                </div>
+            </div>
+
+            <h1 className="center-text"> DRONE </h1>
+        </div>
+    </Fade>
   )
 }
 

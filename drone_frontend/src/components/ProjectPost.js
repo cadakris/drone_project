fc7104d095d71c56aa66af8cd6a3dab6657a698f
Kristen <kriscadacio@gmail.com>
2025-01-10 16:46:34 -0500

    Styled project posts

--- drone_frontend/src/components/ProjectPost.js
+++ drone_frontend/src/components/ProjectPost.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const ProjectPosts = ({ post }) => {
   return (
-    <div className="video p-4 bg-white shadow-md rounded-lg flex flex-col items-center">
+    <div className="video p-4 bg-white rounded-lg flex flex-col items-center">
       <h3 className="text-lg font-bold mb-3">{post.project_title}</h3>
       <video
         src={post.video}
         controls
-        className="w-full h-48 mb-3 rounded-md"
+        className="mb-3"
       />
       <p className="text-sm text-gray-600">{post.description}</p>
     </div>

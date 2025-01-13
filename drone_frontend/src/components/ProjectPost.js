54d3add9ee9b9b738a03d1021c75879f00c2c5b1
Kristen <kriscadacio@gmail.com>
2025-01-13 16:00:32 -0500

    Fixed the modal so that when you click on the drone footage you can close out of the pop-up

--- drone_frontend/src/components/ProjectPost.js
+++ drone_frontend/src/components/ProjectPost.js
@@ -9,6 +9,7 @@ const ProjectPosts = ({ post }) => {
   };
 
   const closeModal = () => {
+    console.log("Close modal called")
     setIsModalOpen(false);
   };
 
@@ -29,31 +30,29 @@ const ProjectPosts = ({ post }) => {
           <h3 className="text-lg font-bold">{post.project_title}</h3>
           <p className="text-sm">{post.description}</p>
           <div className="flex justify-center p-6">
-            <button  className="w-16 h-16 bg-neutral-300 text-black rounded-full flex items-center justify-center hover:shadow-lg hover:scale-110 transition-transform duration-200">Play</button>
+            <button
+                onClick={openModal}
+                className="w-16 h-16 bg-neutral-300 text-black rounded-full flex items-center justify-center hover:shadow-lg hover:scale-110 transition-transform duration-200">Play</button>
           </div>
 
         </div>
       </div>
 
-      {/* Modal for Fullscreen Video */}
       {isModalOpen && (
-        <div className="modal fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50">
-          <div className="relative w-full max-w-4xl">
-            {/* Close Button */}
-            <button
-              className="absolute top-2 right-2 text-white text-2xl"
-              onClick={closeModal}
-            >
-              ✕
-            </button>
-            {/* Fullscreen Video */}
-            <video
-              src={post.video}
-              className="w-full h-auto rounded-lg"
-              autoPlay
-              controls
-            />
-          </div>
+        <div className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50">
+            <div className="relative w-full max-w-4xl z-50">
+                <button
+                onClick={closeModal}
+                className="absolute top-2 right-2 bg-black bg-opacity-70 text-white hover:shadow-lg hover:scale-110 transition-transform duration-200 rounded-full px-4 py-2 z-50">
+                ✕
+                </button>
+                <video
+                src={post.video}
+                className="w-full h-auto"
+                autoPlay
+                controls
+                />
+            </div>
         </div>
       )}
     </div>

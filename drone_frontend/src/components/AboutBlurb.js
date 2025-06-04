08b65ae5690d0a97f57212b5564a3e113160ae03
Kristen <kriscadacio@gmail.com>
2025-06-04 15:08:26 -0400

    AboutBlurb - Added a "Get Started button"

--- drone_frontend/src/components/AboutBlurb.js
+++ drone_frontend/src/components/AboutBlurb.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 function AboutBlurb() {
   return (
-    <section>
-      <div className="w-full h-auto bg-[#384454] p-4 md:p-6 pt-28 md:pt-44">
+    <section id="about">
+      <div className="w-full h-auto bg-[#384454] p-6 md:p-6 pt-28 md:pt-44">
         <h1 className="text-[#317AC1] text-center mb-6 text-2xl md:text-4xl lg:text-6xl">
           Aerial Drone Solutions
         </h1>
@@ -14,6 +14,13 @@ function AboutBlurb() {
           <p className="text-white text-base sm:text-lg md:text-xl lg:text-2xl text-center px-4 sm:px-12 md:px-24">
             Capturing the action, ensuring precision: FPV drones for extreme sports, stabilized drones for inspection and photogrammetry.
           </p>
+
+        </div>
+
+        <div className="flex justify-center p-2">
+          <button className="w-24 md:w-28 lg:w-36 h-12 md:h-12 lg:h-16 bg-[#ad956b] rounded-xl text-white text-base md:text-lg lg:text-xl hover:bg-pink-600">
+            Get Started
+          </button>
         </div>
       </div>
     </section>

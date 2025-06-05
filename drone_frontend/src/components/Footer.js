de52dabd2e9b336fc66c6b9774a58a2fc6221902
Kristen <kriscadacio@gmail.com>
2025-06-05 17:15:39 -0400

    Changing the layout of the footer again

--- drone_frontend/src/components/Footer.js
+++ drone_frontend/src/components/Footer.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import WhatsApp from '../images/WhatsApp.png';
-import Email from '../images/Email.png';
+import Email2 from '../images/Email2.png';
 import Instagram from '../images/Instagram.png'
-// import { HiOutlineMail } from "react-icons/hi";
-import { IoIosArrowForward } from "react-icons/io";
+import YouTube from '../images/YouTube.png'
 import logoNoBackground from '../images/logoNoBackground.png';
+import { IoIosArrowForward } from "react-icons/io";
 
 function Footer() {
   return (
   <>
-<div className="w-full h-auto bg-[#384454] m-0 p-0 overflow-hidden">
-  <div className="flex items-center justify-center space-x-10">
+  <div className="bg-[#384454] text-white flex flex-col md:flex-row md:items-center">
+    {/* {LEFT-CONTACT} */}
+    <div className="flex items-center justify-center gap-x-2">
+      <p className="text-white pr-12">SEEN <br/> ENOUGH?</p>
+      <p className="text-[#317AC1] gap-2 text-2xl md:text-4xl lg:text-7xl">Contact Us</p>
+      
+      <button
+        className="flex items-center justify-center 
+        bg-[#E1A624] bg-opacity-70 text-white 
+        text-2xl md:text-3xl lg:text-4xl 
+        hover:shadow-lg hover:scale-110 transition-transform duration-200 
+        rounded-full 
+        w-8 h-8 md:w-10 md:h-10 lg:w-16 lg:h-16 z-50"
+      >
+        <IoIosArrowForward />
+      </button>
+    </div>
+
+    {/* {RIGHT-SOCIALS} */}
+    <div className="flex flex-col space-y-4 items-end mt-8 md:mt-0">
+
+    </div>
+
+
+  </div>
+{/* <div className="w-full h-auto bg-[#384454] mt-0 pt-8 pb-4 overflow-hidden">
+  <div className="flex space-x-10">
     <p className="text-white pr-24">SEEN <br/> ENOUGH?</p>
     <p className="text-[#317AC1] text-2xl md:text-4xl lg:text-7xl">Contact Us</p>
     <button
@@ -25,9 +50,9 @@ function Footer() {
     </button>
   </div>
   
-  <div className="flex items-center justify-center space-x-10">
+  <div className="flex items-center justify-center space-y-10 pb-8 space-x-12">
     <p className="text-white pr-24">WANT <br/> MORE?</p>
-     <ul className="flex space-x-10">
+     <ul className="flex space-x-16">
       <li>
         <a 
           href="https://wa.me/1234567890" 
@@ -35,7 +60,7 @@ function Footer() {
           rel="noopener noreferrer"
           className="inline-block"
         >
-          <div className="h-24 w-24 overflow-hidden flex items-center justify-center rounded-full">
+          <div className="h-12 w-12 md:h-16 md:w-16 rounded-full">
             <img src={WhatsApp} alt="WhatsApp" className="h-full w-full object-cover" />
           </div>
         </a>
@@ -48,8 +73,8 @@ function Footer() {
           rel="noopener noreferrer"
           className="inline-block"
         >
-          <div className="h-20 w-20 overflow-hidden flex items-center justify-center rounded-full">
-            <img src={Email} alt="Email link" className="h-full w-full object-cover" />
+          <div className="h-12 w-12 md:h-16 md:w-16 overflow-hidden rounded-full">
+            <img src={Email2} alt="Email link" className="h-full w-full object-cover" />
           </div>
         </a>
       </li>
@@ -61,27 +86,27 @@ function Footer() {
           rel="noopener noreferrer"
           className="inline-block"
         >
-          <div className="h-20 w-20 overflow-hidden flex items-center justify-center rounded-full">
+          <div className="h-12 w-12 md:h-16 md:w-16 overflow-hidden  rounded-full">
             <img src={Instagram} alt="Instagram link" className="h-full w-full object-cover" />
           </div>
         </a>
       </li>
 
-            <li>
+      <li>
         <a
           href=""
           target="_blank"
           rel="noopener noreferrer"
           className="inline-block"
         >
-          <div className="h-20 w-20 overflow-hidden flex items-center justify-center rounded-full">
-            <img src={Instagram} alt="Instagram link" className="h-full w-full object-cover" />
+          <div className="h-12 w-12 md:h-16 md:w-16 overflow-hidden rounded-full">
+            <img src={YouTube} alt="YouTube Link" className="h-full w-full object-cover" />
           </div>
         </a>
       </li>
     </ul>
   </div>
-</div>
+</div> */}
 
   </>
     

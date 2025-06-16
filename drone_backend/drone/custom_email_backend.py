96b0c7a2db1c829b0a5438f572f4a0b3cbbdcb0b
Kristen <kriscadacio@gmail.com>
2025-06-16 14:01:14 -0700

    Changed custom email backend file

--- drone_backend/drone/custom_email_backend.py
+++ drone_backend/drone/custom_email_backend.py
@@ -3,6 +3,7 @@ import certifi
 from django.core.mail.backends.smtp import EmailBackend
 
 class CustomEmailBackend(EmailBackend):
-    def __init__(self, *args, **kwargs):
-        kwargs['ssl_context'] = ssl.create_default_context(cafile=certifi.where())
-        super().__init__(*args, **kwargs)
+    def open(self):
+        # Force certifi SSL context before opening connection
+        self.ssl_context = ssl.create_default_context(cafile=certifi.where())
+        return super().open()

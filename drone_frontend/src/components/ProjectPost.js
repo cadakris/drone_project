e132716f389f70c0a5b2a37f7938b9837d767668
Kristen <kriscadacio@gmail.com>
2025-06-27 12:26:02 -0400

    Fixed the look and feel of the gallery and the sizing of the videos of the gallery

--- drone_frontend/src/components/ProjectPost.js
+++ drone_frontend/src/components/ProjectPost.js
@@ -17,25 +17,21 @@ const ProjectPosts = ({ post }) => {
     <section>
           <div className="relative video-container p-4 overflow-hidden">
       {/* Video Thumbnail with Play Button */}
-      <div
-        className="video-thumbnail relative cursor-pointer group"
-        onClick={openModal}
-      >
-        <video
-          src={post.video}
-          className="w-full h-48 object-cover  pointer-events-none"
-          muted
-        />
+<div className="relative w-full aspect-video cursor-pointer group" onClick={openModal}>
+  <video
+    src={post.video}
+    className="w-full h-full object-cover pointer-events-none"
+    muted
+  />
         {/* Hover Title and Description */}
         <div className="absolute inset-0 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <h3 className="text-lg font-bold">{post.project_title}</h3>
-          <p className="text-sm">{post.description}</p>
+          <h3 className="text-lg font-bold p-1">{post.project_title}</h3>
+          <p className="text-sm p-1">{post.description}</p>
           <div className="flex justify-center p-6">
             <button
                 onClick={openModal}
                 className="w-16 h-16 bg-neutral-300 text-black rounded-full flex items-center justify-center hover:shadow-lg hover:scale-110 transition-transform duration-200">Play</button>
           </div>
-
         </div>
       </div>
 
@@ -44,7 +40,7 @@ const ProjectPosts = ({ post }) => {
             <div className="relative w-full max-w-4xl z-50">
                 <button
                 onClick={closeModal}
-                className="absolute top-2 right-2 bg-black bg-opacity-70 text-white hover:shadow-lg hover:scale-110 transition-transform duration-200 rounded-full px-4 py-2 z-50">
+                className="absolute top-4 right-4 w-10 h-10 flex items-center justify-center bg-black bg-opacity-70 text-white hover:shadow-lg hover:scale-110 transition-transform duration-200 rounded-full z-50">
                 ✕
                 </button>
                 <video

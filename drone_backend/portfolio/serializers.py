a8e0d2a57815c7f9b685553165f4c5e18d340756
Kristen <kriscadacio@gmail.com>
2025-07-14 13:37:40 -0400

    Updated my serializers to reflect changes made to Models

--- drone_backend/portfolio/serializers.py
+++ drone_backend/portfolio/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import ProjectPost
+from .models import About
+from .models import Services
 
 class ProjectPostSerializer(serializers.ModelSerializer):
     class Meta: 
         model = ProjectPost
-        fields = ['project_title', 'image', 'video', 'description', 'project_type'] # You can specify the fields you want to include
+        fields = ['project_title_en', 'project_title_fr', 'video', 'description_en', 'description_fr','project_type']
+
+class AboutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields = ['tagline_en', 'tagline_fr', 'large_description_en','large_description_fr']
+
+class ServicesSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Services
+        fields = ['image', 'title_en', 'title_fr', 'description_en,', 'description_fr']
+        

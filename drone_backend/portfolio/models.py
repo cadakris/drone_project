1fdaa8970952b8fe93b6c5e7b0af2a420b94d407
Kristen <kriscadacio@gmail.com>
2025-07-14 12:29:56 -0400

    Changed the models in order to allow client to be able to make inputs that would render on the Frontend without having to go through me

--- drone_backend/portfolio/models.py
+++ drone_backend/portfolio/models.py
@@ -10,23 +10,35 @@ class ProjectPost(models.Model):
         ('SD', 'SD'),
     ]
 
-    project_title = models.TextField(max_length=50, default="Drone Project")
-    image = models.ImageField(upload_to='project_images/')
+    project_title_en = models.CharField("Title (EN)", max_length=50, default="Title goes here")
+    project_title_fr = models.CharField("Title (FR)", max_length=50, blank=True, null=True)
     video = models.URLField(blank=True, null=True)
-    description = models.TextField()
+    description_en = models.TextField("Description (EN)", default="Description goes here")
+    description_fr = models.TextField("Description (FR)", blank=True, null=True)
     project_type = models.CharField(max_length=3, choices=PROJECT_TYPE_CHOICES)
 
     def __str__(self):
-        return self.project_title
+        return self.project_title_en
 
-
+1
 class About(models.Model):
-    title = models.CharField(max_length=500)
-    image = models.ImageField(upload_to='about_images/')
-    description = models.TextField()
+    tagline_en = models.CharField(default="Tagline goes here")
+    tagline_fr = models.CharField(blank=True, null=True)
+    large_description_en = models.TextField(default="Description goes here")
+    large_description_fr = models.TextField(blank=True, null=True)
+
+
+
+class Services(models.Model):
+    image = models.URLField(blank=True, null=True)
+    title_en = models.CharField(max_length=50)
+    title_fr = models.CharField(max_length=50, blank=True, null=True)
+    description_en = models.TextField(default="Description goes here")
+    description_fr = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.title_en
+
 
 # def project_posts_list(request):
 #     # Query all ProjectPost objects and convert to a list of dictionaries
